fdabca54cd2acf439fa66fb89e8a09d9a5d999b1
Mrinal Kanti Sardar <mrinal.k.sardar@gmail.com>
2022-06-03 07:59:14 +0530

    script to fix coverage report

---
+++ ci/scripts/run_tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $PARALLEL == 'true' ]]; then
+    export XTRATESTARGS="-n4 $XTRATESTARGS"
+fi
+
+if [[ $COVERAGE == 'true' ]]; then
+    export XTRATESTARGS="--cov=dask --cov-report=xml $XTRATESTARGS"
+fi
+
+echo "py.test -v tests --runslow $XTRATESTARGS"
+py.test -v tests --runslow $XTRATESTARGS
+
+set +e
